52ae19a127f7625d08c6a142fd068bca6ac4d3d5
mac-mac-mac <macfbarron@gmail.com>
2023-09-09 20:59:33 +0000

    Finalized API integration and styling

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template      # import flask framework
-app = Flask(__name__)                         # create an app instance
+import urllib.request, json
 
-@app.route("/")                               # use the home url
-def hello():                                  # method called hello
-    return render_template("index.html")      # returns "hello world"
-if __name__ == "__main__":                    # when running python app.py
-    app.run(debug=True)                       # run the flask app
+from flask import Flask, render_template, request   # import flask framework
+app = Flask(__name__)                               # create an app instance
 
-@app.route("/<name>")                         # route with URL variable /<name>
-def hello_name(name):                         # call method hello_name
-    return "Hello "+ name                     # which returns "hello + name
+@app.route("/")                                     # use the home url
+def hello():                                        # method called hello
+    url = "https://xkcd.com/info.0.json"
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+    return render_template("index.html", datum=dict)
 
-@app.route("/about")                
-def about():                    
-    return render_template("about.html")   
+@app.route("/<name>")                               # route with URL variable /<name>
+def hello_name(name):                               # call method hello_name
+    return "Hello "+ name                           # which returns "hello + name
+
+@app.route("/about")
+def about():
+    name = request.args.get('name') if request.args.get('name') else "Mac" 
+    return render_template("about.html", aboutName=name) 
+
+if __name__ == "__main__":                          # when running python app.py
+    app.run(debug=True)                             # run the flask app
